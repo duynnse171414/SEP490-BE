b1aacc04ea9d219f670ce08e5a0650e1039ffc4f
HienDamHai <hiendamhai12b6@gmail.com>
2025-03-26 09:38:57 +0700

    useApproveClaims

--- src/pages/claims/ClaimsPage.tsx
+++ src/pages/claims/ClaimsPage.tsx
@@ -1,7 +1,5 @@
-import { putData } from "@/api/fetchers";
 import { ViewClaimRqCard } from "@/components/common/ViewClaimRqCard";
-import { useClaimsByPM } from "@/features/claims/hooks/useClaims";
-import { ClaimStatus } from "@/features/claims/types";
+import { useApproveClaims, useClaimsByPM } from "@/features/claims/hooks/useClaims";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { CheckCircle2, Loader2 } from "lucide-react";
@@ -10,44 +8,17 @@ const ClaimsPage = () => {
   const { data, isLoading, mutate } = useClaimsByPM({ status: 2 });
   const claims = data?.data;
   const [selectedClaim, setSelectedClaim] = useState<string[]>([]);
-  const [isApproving, setIsApproving] = useState(false);
-
-  console.log("Claims =", claims);
-  const handleApprove = (staffId: string) => {
-    console.log(`Approved claim for staff ${staffId}`);
-  };
-
-  const handleReject = (staffId: string) => {
-    console.log(`Rejected claim for staff ${staffId}`);
-  };
+  const { approveSelectedClaims, isApproving } = useApproveClaims();
 
   const handleSelect = (claimId: string, checked: boolean) => {
     setSelectedClaim((prevSelected) =>
-      checked
-        ? [...prevSelected, claimId]
-        : prevSelected.filter((id) => id !== claimId)
+      checked ? [...prevSelected, claimId] : prevSelected.filter((id) => id !== claimId)
     );
   };
 
-  const handleApproveSelected = async () => {
-    if (selectedClaim.length === 0) return;
-    setIsApproving(true);
-
-    console.log("Approving claims:", selectedClaim);
-
-    try {
-      const response = await putData("/ClaimRequests/approve", selectedClaim);
-      console.log("API response:", response);
-      setSelectedClaim([]);
-    } catch (error: any) {
-      console.error(
-        "Error approving claims:",
-        error.response?.data || error.message
-      );
-    } finally {
-      setIsApproving(false);
-      await mutate();
-    }
+  const handleApproveSelected = () => {
+    approveSelectedClaims(selectedClaim);
+    setSelectedClaim([]);
   };
 
   if (isLoading) {
