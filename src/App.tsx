cf8bf77a5bb7c0398fb7258061812322b34bc1da
leebni <daitvqse184402@fpt.edu.vn>
2025-03-31 21:28:17 +0700

    merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "react-hot-toast";
 import { AppRouter } from "./routers";
 import { ThemeProvider } from "next-themes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "sonner";
+// import { Toaster } from "sonner";
 
 // Tạo một instance của QueryClient
 const queryClient = new QueryClient();
