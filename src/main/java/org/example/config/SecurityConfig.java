ab92354cbf7432e260494c69b51b2c50c2bff547
duynnse171414 <duynnse171414@fpt.edu.vn>
2026-03-19 19:45:18 +0700

    Update CORS configuration to allow all origins

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // URL của frontend
+                        .allowedOriginPatterns("*") // URL của frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
