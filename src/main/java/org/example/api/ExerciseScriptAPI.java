8874f0739309ec6e1cac4227fe53d1c4a45ff090
duynnse171414 <duynnse171414@fpt.edu.vn>
2026-04-09 23:04:49 +0700

    Update ExerciseScriptAPI.java

--- src/main/java/org/example/api/ExerciseScriptAPI.java
+++ src/main/java/org/example/api/ExerciseScriptAPI.java
@@ -6,6 +6,7 @@ import org.example.service.ExerciseScriptService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import jakarta.persistence.Column;
 
 import java.util.List;
 
